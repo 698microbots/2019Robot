684959e85ed4cbe839f71f7cc0bec14198bb18db
ben <chittendent@msn.com>
2019-01-02 10:24:03 -0700

    first commit

---
+++ src/org/usfirst/frc/team698/robot/subsystems/test.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team698.robot.subsystems;
+
+public class test {
+
+}
---
+++ src/org/usfirst/frc/team698/robot/subsystems/test2.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team698.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class test2 extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
