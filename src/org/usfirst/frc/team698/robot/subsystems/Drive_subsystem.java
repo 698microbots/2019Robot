b42c19d3bbe6d73d40c3a0a43a89c0548891fc38
ben <chittendent@msn.com>
2019-01-02 11:40:00 -0700

    added basic drive system functionality

--- src/org/usfirst/frc/team698/robot/subsystems/Drive_subsystem.java
+++ src/org/usfirst/frc/team698/robot/subsystems/Drive_subsystem.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team698.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 
 /**
  *
@@ -9,10 +12,38 @@ public class Drive_subsystem extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	private static final Victor driveRightRearMotor = new Victor(RobotMap.driveRightRearMotor);
+	private static final Victor driveRightFrontMotor = new Victor(RobotMap.driveRightFrontMotor);
+	private static final Victor driveLeftRearMotor = new Victor(RobotMap.driveLeftRearMotor);
+	private static final Victor driveLeftFrontMotor = new Victor(RobotMap.driveLeftFrontMotor);
+	
+	private static final Encoder driveRightEncoder = new Encoder(RobotMap.driveRightEncoderChannelA, RobotMap.driveRightEncoderChannelB, rightEncoderInverted);
+	private static final Encoder driveLeftEncoder = new Encoder(RobotMap.driveLeftEncoderChannelA, RobotMap.driveLeftEncoderChannelB, leftEncoderInverted);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    /**
+	 * Set the speed of the two right motors
+	 * @param speed between -1 and 1
+	 */
+	public void setRightSpeed(double speed) {
+		if(speed<-1) speed =-1;
+		if(speed>1) speed=1;
+		driveRightRearMotor.set(speed);
+		driveRightFrontMotor.set(speed);
+	}
+	
+	/**
+	 * Set the speed of the two left motors
+	 * @param speed between -1 and 1
+	 */
+	public void setLeftSpeed(double speed) {
+		if(speed<-1) speed =-1;
+		if(speed>1) speed=1;
+		driveLeftRearMotor.set(speed);
+		driveLeftFrontMotor.set(speed);
+	}
 }
 
